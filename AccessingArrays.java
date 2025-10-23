59571e8135c86914faac30b3e241724687c1418b
Jemimah-Baraka <jemimahbaraka32@gmail.com>
2025-10-23 16:02:01 +0300

    Add files via upload

---
+++ AccessingArrays.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.accessingarrays;
+
+/**
+ *
+ * @author HP
+ */
+public class AccessingArrays {
+
+    public static void main(String[] args) {
+        int[] year = {1907, 1902, 1922};
+        System.out.println(year[0]);
+        System.out.println(year[1]);
+        System.out.println(year[2]);
+    }
+}
