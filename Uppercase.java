86e7fd890c7903e44ebe01045e90ab3ad632814f
Jemimah-Baraka <jemimahbaraka32@gmail.com>
2025-10-31 10:27:28 +0300

    Add files via upload

---
+++ Uppercase.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uppercase;
+
+/**
+ *
+ * @author HP
+ */
+public class Uppercase {
+
+    public static void main(String[] args) {
+        String name = "Jemimah";
+                System.out.println(name.toUpperCase());
+    }
+}
