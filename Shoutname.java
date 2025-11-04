f044122adeedf1e733eb50dbc7a81d9fce82c10e
Jemimah-Baraka <jemimahbaraka32@gmail.com>
2025-11-04 23:31:22 +0300

    Add files via upload

---
+++ Shoutname.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.method2;
+
+/**
+ *
+ * @author HP
+ */
+public class Method2 {
+
+    public static void main(String[] args) {
+        shout("Anita");
+    }
+        public static void shout(String name){
+            System.out.println(name.toUpperCase()+"!");
+        }
+    
+}
