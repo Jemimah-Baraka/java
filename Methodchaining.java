a98b9000cc44778a1e1eb7bb191dbf82bfe888d6
Jemimah-Baraka <jemimahbaraka32@gmail.com>
2025-11-12 01:28:41 +0300

    Add files via upload

---
+++ Methodchaining.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.methodchaining;
+
+/**
+ *
+ * @author HP
+ */
+public class Methodchaining {
+
+    public static void main(String[] args) {
+        String message = "Hello World";
+        String input = "hello world";
+        System.out.println(message.toLowerCase().equals(input));
+    }
+}
