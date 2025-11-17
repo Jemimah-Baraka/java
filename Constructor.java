10537b8118cad78dfdccc1c06ef52a186033f2af
Jemimah-Baraka <jemimahbaraka32@gmail.com>
2025-11-17 23:35:03 +0300

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.constructor;
+
+/**
+ *
+ * @author HP
+ */
+public class Constructor {
+    
+static class Car{
+    String color;
+    String model;
+    int year;
+    
+Car(String color, String model, int year){
+    this.color = color;
+    this.model = model;
+    this.year = year;
+}
+}
+
+    public static void main(String[] args) {
+        Car myCar = new Car("blue", "Corolla", 2022);
+        System.out.println(myCar.year);
+    }
+}
